432e59fd90911e5a43a5c12b506f20dfbd56d70e
Brian Kemboi <brnkemboi@gmail.com>
2024-06-27 10:13:26 +0300

    update file

--- server/app.py
+++ server/app.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
